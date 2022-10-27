927e6c9fb6db7232b951798ff14b7024e13db43c
biagang <biag.ang87@gmail.com>
2022-10-27 17:20:07 +0400

    accepted base extended to [2,36] (#5)

--- src/util.rs
+++ src/util.rs
@@ -18,6 +18,26 @@ pub mod literals {
     pub const _D: u8 = 'd' as u8;
     pub const _E: u8 = 'e' as u8;
     pub const _F: u8 = 'f' as u8;
+    pub const _G: u8 = 'g' as u8;
+    pub const _H: u8 = 'h' as u8;
+    pub const _I: u8 = 'i' as u8;
+    pub const _J: u8 = 'j' as u8;
+    pub const _K: u8 = 'k' as u8;
+    pub const _L: u8 = 'l' as u8;
+    pub const _M: u8 = 'm' as u8;
+    pub const _N: u8 = 'n' as u8;
+    pub const _O: u8 = 'o' as u8;
+    pub const _P: u8 = 'p' as u8;
+    pub const _Q: u8 = 'q' as u8;
+    pub const _R: u8 = 'r' as u8;
+    pub const _S: u8 = 's' as u8;
+    pub const _T: u8 = 't' as u8;
+    pub const _U: u8 = 'u' as u8;
+    pub const _V: u8 = 'v' as u8;
+    pub const _W: u8 = 'w' as u8;
+    pub const _X: u8 = 'x' as u8;
+    pub const _Y: u8 = 'y' as u8;
+    pub const _Z: u8 = 'z' as u8;
     pub const _EXCL: u8 = '!' as u8;
     pub const _STAR: u8 = '*' as u8;
 }
